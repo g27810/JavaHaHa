be122c00685155786f6256e2933604a88275eb95
g27810 <51378973+g27810@users.noreply.github.com>
2019-06-30 22:57:36 +0800

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 public class Demo{
   public static void main(String[] args){
-    System.out.println("Hello GitHub!!!");  
+    System.out.println("Hello GitHub!!!"); 
+    //这是我新添加的一行
+    System.out.println("在GitHub好好学习,争取不断学习进步");
   }
 }
